aa395ea49e2bea193b5296304ff8cc0c10e12322
Shubham6822 <sharmashubham6822@gmail.com>
2025-04-22 18:06:37 +0530

    Initializes the project with MCP server

---
+++ index.ts
@@ -0,0 +1,7 @@
+import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
+
+const server = new McpServer({
+  name: "Tastymonial",
+  description: "A platform for sharing and discovering delicious recipes.",
+  version: "0.0.1",
+});
